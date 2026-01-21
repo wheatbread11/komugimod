954406e39a99df039e8fa2a6348e70eb0d6470ce
komugi_bread <kazuya1101.personal@gmail.com>
2026-01-21 20:35:12 +0900

    Initial commit

---
+++ src/main/java/com/wheatbread11/examplemod/ExampleMod.java
@@ -0,0 +1,13 @@
+package com.wheatbread11.examplemod;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(ExampleMod.MODID)
+public class ExampleMod {
+    public static final String MODID = "examplemod";
+
+    public ExampleMod(IEventBus modEventBus) {
+        
+    }
+}
