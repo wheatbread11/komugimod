b2085354974ab05d48aabaea4a70b213e47a711b
komugi_bread <kazuya1101.personal@gmail.com>
2026-01-21 20:42:41 +0900

    First settings

---
+++ src/main/java/com/wheatbread11/komugimod/KomugiMod.java
@@ -0,0 +1,13 @@
+package com.wheatbread11.komugimod;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(KomugiMod.MODID)
+public class KomugiMod {
+    public static final String MODID = "komugimod";
+
+    public KomugiMod(IEventBus modEventBus) {
+        
+    }
+}
