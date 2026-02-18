b540698287592c57bad61897322c806d5f43a594
komugi_bread <kazuya1101.personal@gmail.com>
2026-02-19 00:29:17 +0900

    Stargazer becomes neutral

--- src/main/java/net/wheatbread11/komugimod/entity/StargazerEntity.java
+++ src/main/java/net/wheatbread11/komugimod/entity/StargazerEntity.java
@@ -1,28 +1,25 @@
 package net.wheatbread11.komugimod.entity;
 
 import java.util.Optional;
+import java.util.UUID;
 import java.util.function.Function;
 
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.util.TimeUtil;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.AnimationState;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.goal.FloatGoal;
-import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
-import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
-import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
-import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
+import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.ResetUniversalAngerTargetGoal;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
@@ -31,22 +28,24 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ExplosionDamageCalculator;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.SimpleExplosionDamageCalculator;
+import org.jetbrains.annotations.Nullable;
 
-public class StargazerEntity extends Monster{
-    private static final int SPAWN_PROTECTION_DURATION = 20;
+public class StargazerEntity extends Monster implements NeutralMob {
     private static final float BLAST_DISTANCE = 2.4f;
-    private static final float BLAST_RADIUS = 3.6f;
-    private static final float BLAST_KNOCKBACK = 3.6f;
+    private static final float BLAST_RADIUS = 2.0f;
+    private static final float BLAST_KNOCKBACK = 2.4f;
     private static final ExplosionDamageCalculator EXPLOSION_DAMAGE_CALCULATOR = new SimpleExplosionDamageCalculator(
         true,
         true,
         Optional.of(BLAST_KNOCKBACK),
         BuiltInRegistries.BLOCK.getTag(BlockTags.AIR).map(Function.identity())
     );
+    private static final UniformInt PERSISTENT_ANGER_TIME = TimeUtil.rangeOfSeconds(20, 39);;
+    private int remainingPersistentAngerTime;
+    @Nullable
+    private UUID persistentAngerTarget;
 
     public final AnimationState idleAnimationState = new AnimationState();
-    
-    private int spawnProtection = SPAWN_PROTECTION_DURATION;
 
     public StargazerEntity(EntityType<? extends Monster> entityType, Level level) {
         super(entityType, level);
@@ -55,12 +54,13 @@ public class StargazerEntity extends Monster{
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(1, new FloatGoal(this));
-        this.goalSelector.addGoal(4, new MeleeAttackGoal(this, 1.0, false));
+        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, (double)1.0F, false));
         this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 0.8));
         this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 8.0F));
         this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
-        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Player.class, true));
-        this.targetSelector.addGoal(2, new HurtByTargetGoal(this));
+        this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, 10, true, false, this::isAngryAt));
+        this.targetSelector.addGoal(3, new ResetUniversalAngerTargetGoal<>(this, true));
     }
 
     public static AttributeSupplier.Builder createAttributes() {
@@ -68,21 +68,20 @@ public class StargazerEntity extends Monster{
             .add(Attributes.MAX_HEALTH, 16.0)
             .add(Attributes.MOVEMENT_SPEED, 0.4)
             .add(Attributes.FOLLOW_RANGE, 24.0)
-            .add(Attributes.ATTACK_DAMAGE, 2.0);
+            .add(Attributes.ATTACK_DAMAGE, 1.0);
     }
 
     @Override
     public void tick() {
-        if (!this.level().isClientSide()) {
-            if (spawnProtection > 0) {
-                spawnProtection--;
+        if (this.isAlive()) {
+            if (this.wasOnFire) {
+                this.explode();
             }
-
-            if (this.isAlive() && spawnProtection <= 0) {
-                if (this.isOnFire()) {
-                    this.explode();
-                }
-                if (this.level().hasNearbyAlivePlayer(this.getX(), this.getY(), this.getZ(), BLAST_DISTANCE)) {
+            var target = this.getTarget();
+            if (target != null) {
+                var targetPos = target.getPosition(0.0F);
+                var thisPos = this.getPosition(0.0F);
+                if (targetPos.closerThan(thisPos, BLAST_DISTANCE)) {
                     this.explode();
                 }
             }
@@ -129,12 +128,34 @@ public class StargazerEntity extends Monster{
                 this.getZ(),
                 BLAST_RADIUS,
                 true,
-                Level.ExplosionInteraction.NONE,
-                ParticleTypes.EXPLOSION,
-                ParticleTypes.EXPLOSION_EMITTER,
-                SoundEvents.GENERIC_EXPLODE
+                Level.ExplosionInteraction.MOB
             );
         this.triggerOnDeathMobEffects(Entity.RemovalReason.KILLED);
         this.discard();
+    }
+
+    @Override
+    public int getRemainingPersistentAngerTime() {
+        return this.remainingPersistentAngerTime;
+    }
+
+    @Override
+    public void setRemainingPersistentAngerTime(int time) {
+        this.remainingPersistentAngerTime = time;
+    }
+
+    @Override
+    public @Nullable UUID getPersistentAngerTarget() {
+        return this.persistentAngerTarget;
+    }
+
+    @Override
+    public void setPersistentAngerTarget(@Nullable UUID target) {
+        this.persistentAngerTarget = target;
+    }
+
+    @Override
+    public void startPersistentAngerTimer() {
+        this.setRemainingPersistentAngerTime((PERSISTENT_ANGER_TIME.sample(this.random)));
     }
 }
